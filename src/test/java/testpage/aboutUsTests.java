1019e7e5ca1a962124873a07239938cd93a0c0c9
vijay <vijay.kannan@weightwatchers.com>
2021-05-21 16:32:54 -0400

    First commit

---
+++ src/test/java/testpage/aboutUsTests.java
@@ -0,0 +1,40 @@
+package testpage;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+
+public class aboutUsTests {
+	WebDriver driver;
+
+	
+	@Test
+	public void getUrl() {
+	WebDriverManager.chromedriver().setup();
+	
+	ChromeOptions options = new ChromeOptions();
+	options.addArguments("--headless");
+	driver = new ChromeDriver(options);
+	
+	driver.manage().window().maximize();
+	driver.get("http://54.208.29.239:5775/");
+
+	
+	driver.findElement(By.xpath("//a[@id='About Us']")).click();
+	String aboutUsPageContent = driver.getPageSource();
+	System.out.println("aboutUsPageContent :" + aboutUsPageContent);
+	Assert.assertTrue(aboutUsPageContent.contains("about"));
+	driver.quit();
+	
+	
+	}	
+}
+
+
