40812aa2876b5bf3c54f2c2158fb0818e5712512
karivik <83606575+karivik@users.noreply.github.com>
2021-05-22 01:42:23 -0400

    Update aboutUsTests.java

--- src/test/java/testpage/aboutUsTests.java
+++ src/test/java/testpage/aboutUsTests.java
@@ -20,7 +20,7 @@ public class aboutUsTests {
 	WebDriverManager.chromedriver().setup();
 	
 	ChromeOptions options = new ChromeOptions();
-	options.addArguments("--headless","--no-sandbox","--disable-dev-shm-usage");
+	options.addArguments("--headless","--no-sandbox","--disable-dev-shm-usage","start-maximized","disable-infobars","--disable-extensions","--disable-gpu");
 	driver = new ChromeDriver(options);
 	
 	driver.manage().window().maximize();
